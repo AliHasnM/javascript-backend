32ebcced8dd731088bdd6cb28beb6465b8a9c14b
AliHasnM <mughal201burewala@gmail.com>
2024-07-08 11:42:57 +0500

    All Setup Config

--- src/app.js
+++ src/app.js
@@ -16,4 +16,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:8000/api/v1/users${differentRoutes}
+
 export { app };
