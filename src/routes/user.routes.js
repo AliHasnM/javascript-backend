eb95c1f0888ab48fe07dfc8bbb37636cc5da22db
AliHasnM <mughal201burewala@gmail.com>
2024-07-16 10:33:17 +0500

    Complete Access and Refresh Token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshAccessToken,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 // Create a new router instance
 const router = Router();
@@ -21,6 +27,11 @@ router.route("/register").post(
   ]),
   registerUser,
 );
+router.route("/login").post(loginUser);
+
+// SECURED ROUTES
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 // Export the router to be used in other parts of the application
 export default router;
