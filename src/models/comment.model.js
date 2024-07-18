f6ff2db5d2a9ca555a580751dade43084efae20d
AliHasnM <mughal201burewala@gmail.com>
2024-07-18 17:30:02 +0500

    Add Comment, Like, Playlist and Tweet Models

---
+++ src/models/comment.model.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema, plugin } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true },
+);
+
+// Applying the pagination plugin to the schema
+commentSchema.plugin(mongooseAggregatePaginate);
+
+// Creating and exporting the Comment model
+export const Comment = mongoose.model("Comment", commentSchema);
