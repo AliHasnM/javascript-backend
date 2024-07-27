0013f01fbfade872319177bfab3653d709671d93
AliHasnM <mughal201burewala@gmail.com>
2024-07-28 01:49:36 +0500

    Add Health Check Controller Endpoints

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ import likeRouter from "./routes/like.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 
 // Routes declaration
 app.use("/api/v1/users", userRouter);
@@ -39,6 +40,7 @@ app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
 // http://localhost:8000/api/v1/users${differentRoutes}
 
 // Export the configured Express app instance
